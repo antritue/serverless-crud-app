fe7e7cde0c6d7bc8573498930154d705eda7cda3
antritue <antritue@gmail.com>
2023-01-02 16:53:30 +0700

    finish get all notes

--- src/functions/list.js
+++ src/functions/list.js
@@ -1,29 +1,25 @@
-// const db = require('./db')
-// const { PutItemCommand } = require("@aws-sdk/client-dynamodb");
-// const { marshall } = require("@aws-sdk/util-dynamodb");
+const db = require('../utils/db')
+const { ScanCommand } = require("@aws-sdk/client-dynamodb");
+const { unmarshall } = require("@aws-sdk/util-dynamodb");
 
 module.exports.list = async (event) => {
     const response = { statusCode: 200 };
 
-    // try {
-    //     const body = JSON.parse(event.body);
-    //     const params = {
-    //         TableName: process.env.TABLE_NAME,
-    //         Item: marshall(body || {}),
-    //     };
-    // await db.send(new PutItemCommand(params));
+    try {
+        const { Items } = await db.send(new ScanCommand({ TableName: process.env.TABLE_NAME }));
 
         response.body = JSON.stringify({
-            message: "Successfully get all item.",
+            message: "Successfully retrieved all posts.",
+            data: Items.map((item) => unmarshall(item)),
         });
-    // } catch (e) {
-    //     console.error(e);
-    //     response.statusCode = 500;
-    //     response.body = JSON.stringify({
-    //         message: "Failed to create item.",
-    //         errorMessage: e.message,
-    //     });
-    // }
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to retrieve posts.",
+            errorMsg: e.message,
+        });
+    }
 
     return response;
 };
